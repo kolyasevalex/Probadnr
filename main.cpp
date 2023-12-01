8c2188f7c775aed45cefa3d9906e2d3aaf92e8bf
Alexandr Kolyasev <kav7272@mail.ru>
2023-12-01 23:02:43 +0400

    Сократил до main

--- main.cpp
+++ main.cpp
@@ -3,339 +3,17 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
-
-#define min(x,y) ((x)<(y)?(x):(y))//добавил коммент
-#define max(x,y) ((x)>(y)?(x):(y))
-#define dist(x,y) ((x-y)*(x-y))
-
-#define INF 1e20       //Pseudo Infitinte number for this code
-
 using namespace std;
 
-/// Data structure for sorting the query
-typedef struct Index
-    {   double value;
-        int    index;
-    } Index;
 
-/// Data structure (circular array) for finding minimum and maximum for LB_Keogh envolop
-struct deque
-{   int *dq;
-    int size,capacity;
-    int f,r;
-};
-
-
-/// Sorting function for the query, sort by abs(z_norm(q[i])) from high to low
-int comp(const void *a, const void* b)
-{   Index* x = (Index*)a;
-    Index* y = (Index*)b;
-    return abs(y->value) - abs(x->value);   // high to low
-}
-
-/// Initial the queue at the begining step of envelop calculation
-void init(deque *d, int capacity)
-{
-    d->capacity = capacity;
-    d->size = 0;
-    d->dq = (int *) malloc(sizeof(int)*d->capacity);
-    d->f = 0;
-    d->r = d->capacity-1;
-}
-
-/// Destroy the queue
-void destroy(deque *d)
-{
-    free(d->dq);
-}
-
-/// Insert to the queue at the back
-void push_back(struct deque *d, int v)
-{
-    d->dq[d->r] = v;
-    d->r--;
-    if (d->r < 0)
-        d->r = d->capacity-1;
-    d->size++;
-}
-
-/// Delete the current (front) element from queue
-void pop_front(struct deque *d)
-{
-    d->f--;
-    if (d->f < 0)
-        d->f = d->capacity-1;
-    d->size--;
-}
-
-/// Delete the last element from queue
-void pop_back(struct deque *d)
-{
-    d->r = (d->r+1)%d->capacity;
-    d->size--;
-}
-
-/// Get the value at the current position of the circular queue
-int front(struct deque *d)
-{
-    int aux = d->f - 1;
-
-    if (aux < 0)
-        aux = d->capacity-1;
-    return d->dq[aux];
-}
-
-/// Get the value at the last position of the circular queueint back(struct deque *d)
-int back(struct deque *d)
-{
-    int aux = (d->r+1)%d->capacity;
-    return d->dq[aux];
-}
-
-/// Check whether or not the queue is empty
-int empty(struct deque *d)
-{
-    return d->size == 0;
-}
-
-/// Finding the envelop of min and max value for LB_Keogh
-/// Implementation idea is intoruduced by Danial Lemire in his paper
-/// "Faster Retrieval with a Two-Pass Dynamic-Time-Warping Lower Bound", Pattern Recognition 42(9), 2009.
-void lower_upper_lemire(double *t, int len, int r, double *l, double *u)
-{
-    struct deque du, dl;
-
-    init(&du, 2*r+2);
-    init(&dl, 2*r+2);
-
-    push_back(&du, 0);
-    push_back(&dl, 0);
-
-    for (int i = 1; i < len; i++)
-    {
-        if (i > r)
-        {
-            u[i-r-1] = t[front(&du)];
-            l[i-r-1] = t[front(&dl)];
-        }
-        if (t[i] > t[i-1])
-        {
-            pop_back(&du);
-            while (!empty(&du) && t[i] > t[back(&du)])
-                pop_back(&du);
-        }
-        else
-        {
-            pop_back(&dl);
-            while (!empty(&dl) && t[i] < t[back(&dl)])
-                pop_back(&dl);
-        }
-        push_back(&du, i);
-        push_back(&dl, i);
-        if (i == 2 * r + 1 + front(&du))
-            pop_front(&du);
-        else if (i == 2 * r + 1 + front(&dl))
-            pop_front(&dl);
-    }
-    for (int i = len; i < len+r+1; i++)
-    {
-        u[i-r-1] = t[front(&du)];
-        l[i-r-1] = t[front(&dl)];
-        if (i-front(&du) >= 2 * r + 1)
-            pop_front(&du);
-        if (i-front(&dl) >= 2 * r + 1)
-            pop_front(&dl);
-    }
-    destroy(&du);
-    destroy(&dl);
-}
-
-/// Calculate quick lower bound
-/// Usually, LB_Kim take time O(m) for finding top,bottom,fist and last.
-/// However, because of z-normalization the top and bottom cannot give siginifant benefits.
-/// And using the first and last points can be computed in constant time.
-/// The prunning power of LB_Kim is non-trivial, especially when the query is not long, say in length 128.
-double lb_kim_hierarchy(double *t, double *q, int j, int len, double mean, double std, double bsf = INF)
-{
-    /// 1 point at front and back
-    double d, lb;
-    double x0 = (t[j] - mean) / std;
-    double y0 = (t[(len-1+j)] - mean) / std;
-    lb = dist(x0,q[0]) + dist(y0,q[len-1]);
-    if (lb >= bsf)   return lb;
-
-    /// 2 points at front
-    double x1 = (t[(j+1)] - mean) / std;
-    d = min(dist(x1,q[0]), dist(x0,q[1]));
-    d = min(d, dist(x1,q[1]));
-    lb += d;
-    if (lb >= bsf)   return lb;
-
-    /// 2 points at back
-    double y1 = (t[(len-2+j)] - mean) / std;
-    d = min(dist(y1,q[len-1]), dist(y0, q[len-2]) );
-    d = min(d, dist(y1,q[len-2]));
-    lb += d;
-    if (lb >= bsf)   return lb;
-
-    /// 3 points at front
-    double x2 = (t[(j+2)] - mean) / std;
-    d = min(dist(x0,q[2]), dist(x1, q[2]));
-    d = min(d, dist(x2,q[2]));
-    d = min(d, dist(x2,q[1]));
-    d = min(d, dist(x2,q[0]));
-    lb += d;
-    if (lb >= bsf)   return lb;
-
-    /// 3 points at back
-    double y2 = (t[(len-3+j)] - mean) / std;
-    d = min(dist(y0,q[len-3]), dist(y1, q[len-3]));
-    d = min(d, dist(y2,q[len-3]));
-    d = min(d, dist(y2,q[len-2]));
-    d = min(d, dist(y2,q[len-1]));
-    lb += d;
-
-    return lb;
-}
-
-/// LB_Keogh 1: Create Envelop for the query
-/// Note that because the query is known, envelop can be created once at the begenining.
-///
-/// Variable Explanation,
-/// order : sorted indices for the query.
-/// uo, lo: upper and lower envelops for the query, which already sorted.
-/// t     : a circular array keeping the current data.
-/// j     : index of the starting location in t
-/// cb    : (output) current bound at each position. It will be used later for early abandoning in DTW.
-double lb_keogh_cumulative(int* order, double *t, double *uo, double *lo, double *cb, int j, int len, double mean, double std, double best_so_far = INF)
-{
-    double lb = 0;
-    double x, d;
-
-    for (int i = 0; i < len && lb < best_so_far; i++)
-    {
-        x = (t[(order[i]+j)] - mean) / std;
-        d = 0;
-        if (x > uo[i])
-            d = dist(x,uo[i]);
-        else if(x < lo[i])
-            d = dist(x,lo[i]);
-        lb += d;
-        cb[order[i]] = d;
-    }
-    return lb;
-}
-
-/// LB_Keogh 2: Create Envelop for the data
-/// Note that the envelops have been created (in main function) when each data point has been read.
-///
-/// Variable Explanation,
-/// tz: Z-normalized data
-/// qo: sorted query
-/// cb: (output) current bound at each position. Used later for early abandoning in DTW.
-/// l,u: lower and upper envelop of the current data
-double lb_keogh_data_cumulative(int* order, double *tz, double *qo, double *cb, double *l, double *u, int len, double mean, double std, double best_so_far = INF)
-{
-    double lb = 0;
-    double uu,ll,d;
-
-    for (int i = 0; i < len && lb < best_so_far; i++)
-    {
-        uu = (u[order[i]]-mean)/std;
-        ll = (l[order[i]]-mean)/std;
-        d = 0;
-        if (qo[i] > uu)
-            d = dist(qo[i], uu);
-        else
-        {   if(qo[i] < ll)
-            d = dist(qo[i], ll);
-        }
-        lb += d;
-        cb[order[i]] = d;
-    }
-    return lb;
-}
-
-/// Calculate Dynamic Time Wrapping distance
-/// A,B: data and query, respectively
-/// cb : cummulative bound used for early abandoning
-/// r  : size of Sakoe-Chiba warpping band
-double dtw(double* A, double* B, double *cb, int m, int r, double bsf = INF)
-{
-
-    double *cost;
-    double *cost_prev;
-    double *cost_tmp;
-    int i,j,k;
-    double x,y,z,min_cost;
-
-    /// Instead of using matrix of size O(m^2) or O(mr), we will reuse two array of size O(r).
-    cost = (double*)malloc(sizeof(double)*(2*r+1));
-    for(k=0; k<2*r+1; k++)    cost[k]=INF;
-
-    cost_prev = (double*)malloc(sizeof(double)*(2*r+1));
-    for(k=0; k<2*r+1; k++)    cost_prev[k]=INF;
-
-    for (i=0; i<m; i++)
-    {
-        k = max(0,r-i);
-        min_cost = INF;
-
-        for(j=max(0,i-r); j<=min(m-1,i+r); j++, k++)
-        {
-            /// Initialize all row and column
-            if ((i==0)&&(j==0))
-            {
-                cost[k]=dist(A[0],B[0]);
-                min_cost = cost[k];
-                continue;
-            }
-
-            if ((j-1<0)||(k-1<0))     y = INF;
-            else                      y = cost[k-1];
-            if ((i-1<0)||(k+1>2*r))   x = INF;
-            else                      x = cost_prev[k+1];
-            if ((i-1<0)||(j-1<0))     z = INF;
-            else                      z = cost_prev[k];
-
-            /// Classic DTW calculation
-            cost[k] = min( min( x, y) , z) + dist(A[i],B[j]);
-
-            /// Find minimum cost in row for early abandoning (possibly to use column instead of row).
-            if (cost[k] < min_cost)
-            {   min_cost = cost[k];
-            }
-        }
-
-        /// We can abandon early if the current cummulative distace with lower bound together are larger than bsf
-        if (i+r < m-1 && min_cost + cb[i+r+1] >= bsf)
-        {   free(cost);
-            free(cost_prev);
-            return min_cost + cb[i+r+1];
-        }
-
-        /// Move current array to previous array.
-        cost_tmp = cost;
-        cost = cost_prev;
-        cost_prev = cost_tmp;
-    }
-    k--;
-
-    /// the DTW distance is in the last cell in the matrix of size O(m^2) or at the middle of our array.
-    double final_dtw = cost_prev[k];
-    free(cost);
-    free(cost_prev);
-    return final_dtw;
-}
-
-/// Print function for debugging
+//Print function for debugging
 void printArray(double *x, int len)
 {   for(int i=0; i<len; i++)
         printf(" %6.2lf",x[i]);
     printf("\n");
 }
 
-/// If expected error happens, teminated the program.
+// If expected error happens, teminated the program.
 void error(int id)
 {
     if(id==1)
@@ -353,14 +31,14 @@ void error(int id)
     exit(1);
 }
 
-/// Main Function
+// Main Function
 int main(  int argc , char *argv[] )
 {
-    FILE *fp;            /// data file pointer
-    FILE *qp;            /// query file pointer
-    double bsf;          /// best-so-far
-    double *t, *q;       /// data array and query array
-    int *order;          ///new order of the query
+    FILE *fp;            // data file pointer
+    FILE *qp;            // query file pointer
+    double bsf;          // best-so-far
+    double *t, *q;       // data array and query array
+    int *order;          //new order of the query
     double *u, *l, *qo, *uo, *lo,*tz,*cb, *cb1, *cb2,*u_d, *l_d;
 
 
@@ -375,18 +53,18 @@ int main(  int argc , char *argv[] )
     double *buffer, *u_buff, *l_buff;
     Index *Q_tmp;
 
-    /// For every EPOCH points, all cummulative values, such as ex (sum), ex2 (sum square), will be restarted for reducing the floating point error.
+    //For every EPOCH points, all cummulative values, such as ex (sum), ex2 (sum square), will be restarted for reducing the floating point error.
     int EPOCH = 100000;
 
-    /// If not enough input, display an error.
+    // If not enough input, display an error.
     if (argc<=3)
         error(4);
 
-    /// read size of the query
+    // read size of the query
     if (argc>3)
         m = atol(argv[3]);
 
-    /// read warping windows
+    // read warping windows
     if (argc>4)
     {   double R = atof(argv[4]);
         if (R<=1)
@@ -403,11 +81,11 @@ int main(  int argc , char *argv[] )
     if( qp == NULL )
         error(2);
 
-    /// start the clock
+    // start the clock
     t1 = clock();
 
 
-    /// malloc everything here
+    // malloc everything here
     q = (double *)malloc(sizeof(double)*m);
     if( q == NULL )
         error(1);
