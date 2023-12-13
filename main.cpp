0511bfa052dc5ad14e5be2fe8f2f29299aee432e
Alexandr Kolyasev <kav7272@mail.ru>
2023-12-13 19:04:22 +0400

    готовый шаблон

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
@@ -23,13 +24,31 @@ void error(int id)
     else if ( id == 3 )
         printf("ERROR : Can't create Output File!!!\n\n");
     else if ( id == 4 )
-    {
         printf("ERROR : Invalid Number of Arguments!!!\n");
-        printf("Command Usage:  UCR_DTW.exe  data-file  query-file   m   R\n\n");
-        printf("For example  :  UCR_DTW.exe  data.txt   query.txt   128  0.05\n");
-    }
+	else if (id == 5)
+		printf("No digits in file");
+        
+    
     exit(1);
 }
+
+int read_double_numbers(FILE* fp, bool onlycount, vector<double>& a) {
+	double d;
+	char c;
+	int cnt = 0;
+	fseek(fp, 0, SEEK_SET);
+	while (1) {
+		int i = fscanf(fp, "%lf", &d);
+		if (i && isfinite(d)) {
+			if (!onlycount) a[cnt] = d;
+			cnt++;
+		}
+		else fscanf(fp, "%c", &c);
+		if (feof(fp)) break;
+	}
+	return cnt;
+}
+
 
 // Main Function
 int main(  int argc , char *argv[] )
@@ -37,7 +56,7 @@ int main(  int argc , char *argv[] )
     FILE *fp;            // data file pointer
     FILE *qp;            // query file pointer
     double bsf;          // best-so-far
-    double *t, *q;       // data array and query array
+    vector<double> t, q;       // data array and query array
     int *order;          //new order of the query
     double *u, *l, *qo, *uo, *lo,*tz,*cb, *cb1, *cb2,*u_d, *l_d;
 
@@ -65,233 +84,26 @@ int main(  int argc , char *argv[] )
     if( fp == NULL )
         error(2);
 
+
     qp = fopen(argv[2],"r");
     if( qp == NULL )
         error(2);
 
     // start the clock
     t1 = clock();
-
-
-    // malloc everything here
-    q = (double *)malloc(sizeof(double)*m);
-    if( q == NULL )
-        error(1);
+    m= read_double_numbers(fp, true, t);
+	if (!m) 
+		error(5);
+	r = read_double_numbers(qp, true, q);
+	if (!r)
+		error(5);
     
-
-    buffer = (double *)malloc(sizeof(double)*EPOCH);
-    if( buffer == NULL )
-        error(1);
-
-   
-
-
-    /// Read query file
-    bsf = INF;
-    i = 0;
-    j = 0;
-    ex = ex2 = 0;
-
-    while(fscanf(qp,"%lf",&d) != EOF && i < m)
-    {
-        ex += d;
-        ex2 += d*d;
-        q[i] = d;
-        i++;
-    }
-    fclose(qp);
-
-    /// Do z-normalize the query, keep in same array, q
-    mean = ex/m;
-    std = ex2/m;
-    std = sqrt(std-mean*mean);
-    for( i = 0 ; i < m ; i++ )
-         q[i] = (q[i] - mean)/std;
-
-    /// Create envelop of the query: lower envelop, l, and upper envelop, u
-    lower_upper_lemire(q, m, r, l, u);
-
-    /// Sort the query one time by abs(z-norm(q[i]))
-    for( i = 0; i<m; i++)
-    {
-        Q_tmp[i].value = q[i];
-        Q_tmp[i].index = i;
-    }
-    qsort(Q_tmp, m, sizeof(Index),comp);
-
-    /// also create another arrays for keeping sorted envelop
-    for( i=0; i<m; i++)
-    {   int o = Q_tmp[i].index;
-        order[i] = o;
-        qo[i] = q[o];
-        uo[i] = u[o];
-        lo[i] = l[o];
-    }
-    free(Q_tmp);
-
-    /// Initial the cummulative lower bound
-    for( i=0; i<m; i++)
-    {   cb[i]=0;
-        cb1[i]=0;
-        cb2[i]=0;
-    }
-
-    i = 0;          /// current index of the data in current chunk of size EPOCH
-    j = 0;          /// the starting index of the data in the circular array, t
-    ex = ex2 = 0;
-    bool done = false;
-    int it=0, ep=0, k=0;
-    long long I;    /// the starting index of the data in current chunk of size EPOCH
-
-    while(!done)
-    {
-        /// Read first m-1 points
-        ep=0;
-        if (it==0)
-        {   for(k=0; k<m-1; k++)
-                if (fscanf(fp,"%lf",&d) != EOF)
-                    buffer[k] = d;
-        }
-        else
-        {   for(k=0; k<m-1; k++)
-                buffer[k] = buffer[EPOCH-m+1+k];
-        }
-
-        /// Read buffer of size EPOCH or when all data has been read.
-        ep=m-1;
-        while(ep<EPOCH)
-        {   if (fscanf(fp,"%lf",&d) == EOF)
-                break;
-            buffer[ep] = d;
-            ep++;
-        }
-
-        /// Data are read in chunk of size EPOCH.
-        /// When there is nothing to read, the loop is end.
-        if (ep<=m-1)
-        {   done = true;
-        } else
-        {   lower_upper_lemire(buffer, ep, r, l_buff, u_buff);
-
-            /// Just for printing a dot for approximate a million point. Not much accurate.
-            if (it%(1000000/(EPOCH-m+1))==0)
-                fprintf(stderr,".");
-
-            /// Do main task here..
-            ex=0; ex2=0;
-            for(i=0; i<ep; i++)
-            {
-                /// A bunch of data has been read and pick one of them at a time to use
-                d = buffer[i];
-
-                /// Calcualte sum and sum square
-                ex += d;
-                ex2 += d*d;
-
-                /// t is a circular array for keeping current data
-                t[i%m] = d;
-
-                /// Double the size for avoiding using modulo "%" operator
-                t[(i%m)+m] = d;
-
-                /// Start the task when there are more than m-1 points in the current chunk
-                if( i >= m-1 )
-                {
-                    mean = ex/m;
-                    std = ex2/m;
-                    std = sqrt(std-mean*mean);
-
-                    /// compute the start location of the data in the current circular array, t
-                    j = (i+1)%m;
-                    /// the start location of the data in the current chunk
-                    I = i-(m-1);
-
-                    /// Use a constant lower bound to prune the obvious subsequence
-                    lb_kim = lb_kim_hierarchy(t, q, j, m, mean, std, bsf);
-
-                    if (lb_kim < bsf)
-                    {
-                        /// Use a linear time lower bound to prune; z_normalization of t will be computed on the fly.
-                        /// uo, lo are envelop of the query.
-                        lb_k = lb_keogh_cumulative(order, t, uo, lo, cb1, j, m, mean, std, bsf);
-                        if (lb_k < bsf)
-                        {
-                            /// Take another linear time to compute z_normalization of t.
-                            /// Note that for better optimization, this can merge to the previous function.
-                            for(k=0;k<m;k++)
-                            {   tz[k] = (t[(k+j)] - mean)/std;
-                            }
-
-                            /// Use another lb_keogh to prune
-                            /// qo is the sorted query. tz is unsorted z_normalized data.
-                            /// l_buff, u_buff are big envelop for all data in this chunk
-                            lb_k2 = lb_keogh_data_cumulative(order, tz, qo, cb2, l_buff+I, u_buff+I, m, mean, std, bsf);
-                            if (lb_k2 < bsf)
-                            {
-                                /// Choose better lower bound between lb_keogh and lb_keogh2 to be used in early abandoning DTW
-                                /// Note that cb and cb2 will be cumulative summed here.
-                                if (lb_k > lb_k2)
-                                {
-                                    cb[m-1]=cb1[m-1];
-                                    for(k=m-2; k>=0; k--)
-                                        cb[k] = cb[k+1]+cb1[k];
-                                }
-                                else
-                                {
-                                    cb[m-1]=cb2[m-1];
-                                    for(k=m-2; k>=0; k--)
-                                        cb[k] = cb[k+1]+cb2[k];
-                                }
-
-                                /// Compute DTW and early abandoning if possible
-                                dist = dtw(tz, q, cb, m, r, bsf);
-
-                                if( dist < bsf )
-                                {   /// Update bsf
-                                    /// loc is the real starting location of the nearest neighbor in the file
-                                    bsf = dist;
-                                    loc = (it)*(EPOCH-m+1) + i-m+1;
-                                }
-                            } else
-                                keogh2++;
-                        } else
-                            keogh++;
-                    } else
-                        kim++;
-
-                    /// Reduce obsolute points from sum and sum square
-                    ex -= t[j];
-                    ex2 -= t[j]*t[j];
-                }
-            }  //for(i=0; i<ep; i++)
-
-            /// If the size of last chunk is less then EPOCH, then no more data and terminate.
-            if (ep<EPOCH)
-                done=true;
-            else
-                it++;
-        }      //  if (ep<=m-1) else
-    }          //  while (!done)
-
-    i = (it)*(EPOCH-m+1) + ep;
+    q.resize(r);
+    t.resize(m);
+	read_double_numbers(fp, false, t);
+	read_double_numbers(qp, false, q);
     fclose(fp);
-
-    free(q);
-    free(u);
-    free(l);
-    free(uo);
-    free(lo);
-    free(qo);
-    free(cb);
-    free(cb1);
-    free(cb2);
-    free(tz);
-    free(t);
-    free(l_d);
-    free(u_d);
-    free(l_buff);
-    free(u_buff);
-
+	fclose(qp);
     t2 = clock();
     printf("\n");
 
